5362d21e389237bef4c2d6d634f48918492d076d
m.goncharov <m.goncharov@bossgs.org>
2017-06-07 18:06:52 +0300

    next function

---
+++ test/core_spec.js
@@ -0,0 +1,47 @@
+import {List, Map} from 'immutable';
+import {expect} from 'chai';
+
+import {setEntries, next} from '../src/core';
+
+describe('application logic', () => {
+
+    describe('setEntries', () => {
+
+        it('добавляет записи к состоянию', () => {
+            const state = Map();
+            const entries = List.of('Trainspotting', '28 Days Later');
+            const nextState = setEntries(state, entries);
+            expect(nextState).to.equal(Map({
+                entries: List.of('Trainspotting', '28 Days Later')
+            }));
+        });
+
+        it('преобразует в immutable', () => {
+            const state = Map();
+            const entries = ['Trainspotting', '28 Days Later'];
+            const nextState = setEntries(state, entries);
+            expect(nextState).to.equal(Map({
+                entries: List.of('Trainspotting', '28 Days Later')
+            }));
+        });
+
+    });
+
+    describe('далее', () => {
+
+        it('берёт для голосования следующие две записи', () => {
+            const state = Map({
+                entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later')
+                }),
+                entries: List.of('Sunshine')
+            }));
+        });
+
+    });
+
+});
